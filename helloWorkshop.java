59efbe84b13364aa83c127cee8001bbd05b4a909
aniktm <aniktm@users.noreply.github.com>
2018-12-17 13:49:36 +1100

    minor changes after clone

--- helloWorkshop.java
+++ helloWorkshop.java
@@ -1,8 +1,8 @@
 /* Hello Workshop Java Program */
-/* 9-12-12 */
+/* Cloned on 17-12-18 */
 class helloWorkshop  {
     public static void main(String[] args) {
-        System.out.println("Greetings Workshop!");
+        System.out.println("Greetings!");
         System.out.println("This is an example file");
         System.out.println("for use in workshop exercises.");
         System.out.println("This is on the master branch");
@@ -10,6 +10,6 @@ class helloWorkshop  {
         System.out.println("Isn't this exciting?");
         System.out.println("Maybe?");
         System.out.println("Okay then.");
-        System.out.println("Goodbye Workshop!");
+        System.out.println("Goodbye !");
     }
 }
